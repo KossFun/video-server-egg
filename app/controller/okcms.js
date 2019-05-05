a05b238f081a93b87dd219068e5fa05ace4a5931
AOWOAO <935685944@qq.com>
2019-05-06 01:44:55 +0800

    优化代码

--- app/controller/okcms.js
+++ app/controller/okcms.js
@@ -1,23 +1,19 @@
-'use strict';
+"use strict";
 
-const Controller = require('egg').Controller;
+const Controller = require("egg").Controller;
 
 class OkcmsController extends Controller {
   async index() {
-    
     let query = this.ctx.query;
 
-    let data = await this.service.okcms.getVideoList(query['t'], query['p']);
+    let data = await this.service.okcms.getVideoList(query["t"], query["p"]);
     this.ctx.body = data;
   }
 
   async video() {
-
     let date = await this.service.okcms.getVideoType();
     this.ctx.body = date;
   }
-
-
 }
 
 module.exports = OkcmsController;
--- config/config.default.js
+++ config/config.default.js
@@ -18,6 +18,18 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true,
+    },
+    // domainWhiteList: ['http://localhost:8080']
+  };
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+  };
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
